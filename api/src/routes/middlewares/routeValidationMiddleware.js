0ecf776560ab54ea7f9bbb4882d8bacb3772c651
Emeka Osuagwu <emekaosuagwuandela@gmail.com>
2019-06-21 10:04:56 +0100

    unit test

---
+++ api/src/routes/middlewares/routeValidationMiddleware.js
@@ -0,0 +1,37 @@
+module.exports = routeValidation = (req, res, next) => {
+    
+    const { amount, week, base_currency, target_currency} = req.body
+    
+    if(!amount){
+        res.send({
+            message: "field missing",
+            field: 'amount' 
+        })
+    }
+
+    if(!base_currency){
+        res.send({
+            message: "field missing",
+            field: "base_currency" 
+        })
+    }
+
+    if(!target_currency){
+        res.send({
+            message: "field missing",
+            field: "target_currency" 
+        })
+    }
+
+    if(!week){
+        res.send({
+            message: "field missing",
+            field: "week" 
+        })
+    }
+
+    next();
+};
+
+
+
